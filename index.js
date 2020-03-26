d5afaef0d28b6047f6dc56063bcfd0fc5bf66827
Woo Mobil <woomobil@Woo-iMac.local>
2020-03-26 19:51:59 +0300

    mainStateId boş iken bildirimden silme düzenlendi

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ export default class WooStream extends React.Component {
     }
 
     componentWillUnmount = () => {
-        MusicControl.stopControl();
+        if (!this.state.mainStateId)
+            MusicControl.stopControl();
 
         if (opts.stopPreviousPlayers) {
             playerStore.default.removeListener('id', this.controlCurrentPlayer);
